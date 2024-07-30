462dbeeae5741862c6ffb486e4b66afe520b8fb0
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-07-30 16:39:37 +0200

    set questions

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
-console.log('JS OK')
+console.log('JS OK');
+
+//Creare un generatore di password in base a Nome, Cognome e Colore Preferito dell'utente + 21
+
+/*TO DO LIST
+1. Prendere il nostro elemento di interesse dal DOM;
+2. Acquisizione del nome dell'utente;
+3. Acquisizione del cognome dell'utente;
+4. Acquisizione del colore preferito dell'utente;
+5. Aggiungere "21";
+6. Generazione password personalizzata;
+*/ 
+
+/*Fase di preparazione  */ 
+const pickElement = document.getElementById('password-displayer') 
+console.log(pickElement)
+
+
+/*Fase di acquisizione dei dati*/ 
+const name = prompt('Qual è il tuo nome?', 'salvatore')
+
+const lastname = prompt('Qual è il tuo cognome?', 'giacomazzo')
+
+const favColor = prompt('Qual è il tuo colore preferito?', 'grigio')
+/*Fase di elaborazione dei dati */ 
+
+
+/*Produzione risultato */ 
+
